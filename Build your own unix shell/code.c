064c6db9fb81d32ee4f3421bb922d72b64711298
Rupal-IIITD <rupal15081@iiitd.ac.in>
2017-03-05 11:13:48 +0530

    Update code.c

--- Build your own unix shell/code.c
+++ Build your own unix shell/code.c
@@ -32,3 +32,55 @@ int main() {
 			printf("\n");
 			continue;
 		}
+strcpy(arr_history[count],arr);
+		++count;
+		if(strcmp(arr, "exit\n")==0)
+			exit(0);
+		strcpy(arr1, arr);
+		token = strtok(arr1, search);
+		if(strcmp(token, "cd")==0)
+		{
+			search = "\n";
+			token = strtok(NULL, search);
+			chdir(token);
+		}
+		if(strcmp(arr, "history\n")==0)
+		{
+		    FILE * fp;
+		    char * line = NULL;
+		    size_t len = 0;
+		    ssize_t read;
+
+		    fp = fopen("/home/rupal/Desktop/history.txt", "r");
+		    if (fp == NULL)
+		        exit(EXIT_FAILURE);
+		    i=0;
+		    while ((read = getline(&line, &len, fp)) != -1) {
+		        i++;
+		        printf("%s", line);
+		    }
+
+		    fclose(fp);
+			int j;
+			for(j=0;j<count-1;j++)
+			{
+				printf("%d %s",j+i+1, arr_history[j]);
+			}
+		}
+	    char *name[] = {
+	        "/bin/bash",
+	        "-c",
+	        arr,
+	        NULL
+	    };
+	    int pid= fork();
+    	if(pid==0)
+    	{
+	        execvp(name[0], name);
+	        exit(0);
+   		}
+   		else
+   			wait(NULL);
+	}
+	return 0;
+}
